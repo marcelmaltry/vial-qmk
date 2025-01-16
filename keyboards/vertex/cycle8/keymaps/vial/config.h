c52ecbfd90071b8f874d615a3ca0397887329062
Marcel Maltry <mmaltry+github@mailbox.org>
2025-01-16 17:50:56 +0100

    WIP Add support for Cycle8

---
+++ keyboards/vertex/cycle8/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Vial configuration */
+#define VIAL_KEYBOARD_UID {0x63, 0xAE, 0x57, 0x4A, 0x47, 0x96, 0xBE, 0x1C}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
